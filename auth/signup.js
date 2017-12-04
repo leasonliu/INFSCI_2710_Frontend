8601e4dc0f97e928bb4058b6922501b64368e0d9
TonyZ <zyc@gis4.org>
2017-12-04 00:59:52 -0500

    nearly done...

---
+++ auth/signup.js
@@ -0,0 +1,17 @@
+"use strict";
+
+// Disabling form submissions if there are invalid fields
+function set_up_vaild() {
+    window.addEventListener('load', function() {
+        var form = document.getElementById('needs-validation');
+        form.addEventListener('submit', function(event) {
+            if (form.checkValidity() === false) {
+                event.preventDefault();
+                event.stopPropagation();
+            }
+            form.classList.add('was-validated');
+        }, false);
+    }, false);
+}
+
+set_up_vaild();
