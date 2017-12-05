9c32de5165f7fa7501992d469e7fe5cc72a1d32a
TonyZ <zyc@gis4.org>
2017-12-05 03:53:38 -0500

    prepare for AngularJS!

--- index.php
+++ index.php
@@ -2,12 +2,20 @@
 include('header.php');
 ?>
 
-<main role="main" class="container">
-  <div class="mt-3">
-    <h1>Welcome to PittMoments!</h1>
-  </div>
-</main>
+  <main role="main" class="container">
+    <div class="mt-3">
 
-<?php
+      <!-- Change with correct grid/laout and ng-repeat -->
+      <div class="card" style="width: 20rem;">
+        <img class="card-img-top" src="http://sci.pitt.edu/wp-content/uploads/2015/10/Pelechrinis-_-Homepage.jpg" alt="Card image cap">
+        <div class="card-body">
+          <p class="card-text">Pitt SCI!</p>
+        </div>
+      </div>
+
+    </div>
+  </main>
+
+  <?php
 include('footer.php');
 ?>
