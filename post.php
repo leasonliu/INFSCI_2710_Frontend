3443389f4f08a3f3d2ecf23eefe9cc4c59a0e249
TonyZ <zyc@gis4.org>
2017-12-06 18:50:52 -0500

    some post

--- post.php
+++ post.php
@@ -1,23 +1,35 @@
 <?php
 include('header.php');
-
 ?>
 
   <div class="container">
+
     <div class="row">
-
-      <form action="<?php echo(SERVER_PREFIX . '/post'); ?>" method="post" enctype="multipart/form-data">
-        <div class="form-group">
-          <input type="file" class="form-control-file" id="pic_id">
+      <div class="mx-auto card">
+        <div class="card-header bg-primary text-light">
+          Post something
         </div>
-        <div class="form-group">
-          <textarea class="form-control" id="contents" rows="2" placeholder="Say something?"></textarea>
+        <div class="card-body">
+          <h4 class="card-title">Show your moments here!</h4>
+          <form id="new-post" action="<?php echo(SERVER_PREFIX . '/post'); ?>" method="post" enctype="multipart/form-data">
+            <div class="form-group">
+              <label class="custom-file">
+                <input type="file" id="pic_id" class="custom-file-input" onchange="document.getElementById('file-name').innerHTML=this.files[0].name">
+                <span class="custom-file-control" id="file-name"></span>
+              </label>
+            </div>
+            <div class="form-group">
+              <textarea class="form-control" id="contents" rows="2" placeholder="What's your thoughs?"></textarea>
+            </div>
+            <button type="submit" class="btn btn-primary">Post!</button>
+          </form>
         </div>
-        <button type="submit" class="btn btn-primary">Post!</button>
-      </form>
-
+      </div>
     </div>
+
   </div>
+
+  <script src="./js/post.js"></script>
 
   <?php
 include('footer.php');
