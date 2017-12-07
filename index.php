7963ad7351282737d54711e5f27b3863b3c13811
TonyZ <zyc@gis4.org>
2017-12-07 01:16:35 -0500

    New design for home page

--- index.php
+++ index.php
@@ -3,14 +3,20 @@ include('header.php');
 ?>
 
   <main role="main" class="container">
-    <div class="mt-3">
+    <div class="row">
 
-      <!-- Change with correct grid/laout and ng-repeat -->
-      <div class="card" style="width: 20rem;">
-        <img class="card-img-top" src="http://sci.pitt.edu/wp-content/uploads/2015/10/Pelechrinis-_-Homepage.jpg" alt="Card image cap">
-        <div class="card-body">
-          <p class="card-text">Pitt SCI!</p>
+      <div class="col-2"></div>
+      <div class="col-7">
+        <!-- Change with correct grid/laout and ng-repeat -->
+        <div class="card" style="width: 20rem;">
+          <img class="card-img-top" src="http://sci.pitt.edu/wp-content/uploads/2015/10/Pelechrinis-_-Homepage.jpg" alt="Card image cap">
+          <div class="card-body">
+            <p class="card-text">Pitt SCI!</p>
+          </div>
         </div>
+      </div>
+      <div class="col-3">
+        <button type="button" class="btn btn-outline-primary">Primary</button>
       </div>
 
     </div>
